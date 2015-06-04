62c4cccf871996bebaccd503040ac114bb277302
Joe Kent <thedeadlybutter@gmail.com>
2015-06-04 12:40:58 -0400

    Adds sass

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules/dosomething-neue/dist'));
 
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'
+}));
+
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 var server = app.listen(3000, function () {
