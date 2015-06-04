7c0fdcb8a0a31c68e8464f905e2d171227f252be
Joe Kent <thedeadlybutter@gmail.com>
2015-06-04 12:49:45 -0400

    Adds auto reloading

--- app.js
+++ app.js
@@ -13,15 +13,17 @@ app.use(sassMiddleware({
     prefix:  '/prefix'
 }));
 
+var reload = require('reload');
+var http = require('http');
+var server = http.createServer(app);
+reload(server, app);
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(3000, function () {
-
+server.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
